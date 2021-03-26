bc41eeaee912acf106869facbd82b26b8e36b29a
Руслан Савиновский <savinovsky.r@gmail.com>
2021-03-26 16:31:33 +0800

    refactor index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -6,46 +6,56 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import * as React from 'react';
-import * as ReactNative from 'react-native';
+import * as React from "react";
+import * as ReactNative from "react-native";
 
 export type TableContent = {
-    children: TableContent[],
-    mNativePtr: number,
-    pageIdx: number,
-    title: string,
+  children: TableContent[];
+  mNativePtr: number;
+  pageIdx: number;
+  title: string;
 };
 
 interface Props {
-    style?: ReactNative.StyleProp<ReactNative.ViewStyle>,
-    source: object,
-    page?: number,
-    scale?: number,
-    minScale?: number,
-    maxScale?: number,
-    horizontal?: boolean,
-    spacing?: number,
-    password?: string,
-    activityIndicator?: any,
-    activityIndicatorProps?: object,
-    enableAntialiasing?: boolean,
-    enablePaging?: boolean,
-    enableRTL?: boolean,
-    enableAnnotationRendering?: boolean,
-    fitPolicy?: number,
-    trustAllCerts?: boolean,
-    singlePage?: boolean,
-    onLoadProgress?: (percent: number,) => void,
-    onLoadComplete?: (numberOfPages: number, path: string, size: {height: number, width: number}, tableContents?: TableContent[]) => void,
-    onPageChanged?: (page: number, numberOfPages: number) => void,
-    onError?: (error: object) => void,
-    onPageSingleTap?: (page: number, x: number, y: number) => void,
-    onScaleChanged?: (scale: number) => void,
-    onPressLink?: (url: string) => void,
+  style?: ReactNative.StyleProp<ReactNative.ViewStyle>;
+  page?: number;
+  scale?: number;
+  minScale?: number;
+  maxScale?: number;
+  horizontal?: boolean;
+  spacing?: number;
+  password?: string;
+  activityIndicator?: any;
+  activityIndicatorProps?: object;
+  enableAntialiasing?: boolean;
+  enablePaging?: boolean;
+  enableRTL?: boolean;
+  enableAnnotationRendering?: boolean;
+  fitPolicy?: number;
+  trustAllCerts?: boolean;
+  singlePage?: boolean;
+  onLoadProgress?: (percent: number) => void;
+  onLoadComplete?: (
+    numberOfPages: number,
+    path: string,
+    size: { height: number; width: number },
+    tableContents?: TableContent[]
+  ) => void;
+  onPageChanged?: (page: number, numberOfPages: number) => void;
+  onError?: (error: object) => void;
+  onPageSingleTap?: (page: number, x: number, y: number) => void;
+  onScaleChanged?: (scale: number) => void;
+  onPressLink?: (url: string) => void;
+  fileManager: FileManager;
+}
+
+interface FileManager {
+  loadFile: () => Promise<{ fileName: string; filePath: string }>;
+  unlinkFile: (filePath: string) => void;
 }
 
 declare class Pdf extends React.Component<Props, any> {
-    setPage: (pageNumber: number) => void;
+  setPage: (pageNumber: number) => void;
 }
 
 export default Pdf;
