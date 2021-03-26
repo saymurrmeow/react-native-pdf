d97bc2d9e1891310e97fa5a43c449778dbdb4a98
Руслан Савиновский <savinovsky.r@gmail.com>
2021-03-26 16:07:57 +0800

    remove unused lodic

--- index.js
+++ index.js
@@ -38,15 +38,6 @@ import PdfView from "./PdfView";
 export default class Pdf extends Component {
   static propTypes = {
     ...ViewPropTypes,
-    source: PropTypes.oneOfType([
-      PropTypes.shape({
-        uri: PropTypes.string,
-        cache: PropTypes.bool,
-        expiration: PropTypes.number,
-      }),
-      // Opaque type returned by require('./test.pdf')
-      PropTypes.number,
-    ]).isRequired,
     page: PropTypes.number,
     scale: PropTypes.number,
     minScale: PropTypes.number,
@@ -70,7 +61,7 @@ export default class Pdf extends Component {
     onScaleChanged: PropTypes.func,
     onPressLink: PropTypes.func,
     fileManager: PropTypes.shape({
-      processingDownload: PropTypes.func.isRequired,
+      processDownload: PropTypes.func.isRequired,
       unlinkFile: PropTypes.func.isRequired,
     }).isRequired,
 
@@ -118,25 +109,11 @@ export default class Pdf extends Component {
       progress: 0,
       isSupportPDFKit: -1,
     };
-
-    this.lastRNBFTask = null;
   }
 
   componentDidUpdate(prevProps) {
     const nextSource = Image.resolveAssetSource(this.props.source);
     const curSource = Image.resolveAssetSource(prevProps.source);
-
-    if (nextSource.uri !== curSource.uri) {
-      // if has download task, then cancel it.
-      if (this.lastRNBFTask) {
-        this.lastRNBFTask.cancel((err) => {
-          this._loadFromSource(this.props.source);
-        });
-        this.lastRNBFTask = null;
-      } else {
-        this._loadFromSource(this.props.source);
-      }
-    }
   }
 
   componentDidMount() {
@@ -150,208 +127,25 @@ export default class Pdf extends Component {
         }
       });
     }
-    this._loadFromSource(this.props.source);
+    this._loadFile();
   }
 
   componentWillUnmount() {
     this._mounted = false;
-    if (this.lastRNBFTask) {
-      this.lastRNBFTask.cancel((err) => {});
-      this.lastRNBFTask = null;
-    }
+    this._unlinkFile(this.state.path);
   }
 
-  _loadFromSource = (newSource) => {
-    const source = Image.resolveAssetSource(newSource) || {};
+  _loadFile = async () => {
+    this.lastFMTask = this.props.fileManager.processDownload();
 
-    let uri = source.uri || "";
-    // first set to initial state
-    if (this._mounted) {
-      this.setState({ isDownloaded: false, path: "", progress: 0 });
-    }
-    const cacheFile = RNFetchBlob.fs.dirs.CacheDir + "/" + SHA1(uri) + ".pdf";
-
-    if (source.cache) {
-      RNFetchBlob.fs
-        .stat(cacheFile)
-        .then((stats) => {
-          if (
-            !Boolean(source.expiration) ||
-            source.expiration * 1000 + stats.lastModified > new Date().getTime()
-          ) {
-            if (this._mounted) {
-              this.setState({ path: cacheFile, isDownloaded: true });
-            }
-          } else {
-            // cache expirated then reload it
-            this._prepareFile(source);
-          }
-        })
-        .catch(() => {
-          this._prepareFile(source);
-        });
-    } else {
-      this._prepareFile(source);
-    }
+    this.lastFMTask.then((res) => {
+      this.setState({ isDownloaded: true, path: res.filePath });
+    });
   };
 
-  _prepareFile = async (source) => {
+  _unlinkFile = async (filePath) => {
     try {
-      if (source.uri) {
-        let uri = source.uri || "";
-
-        const isNetwork = !!(uri && uri.match(/^https?:\/\//));
-        const isAsset = !!(uri && uri.match(/^bundle-assets:\/\//));
-        const isBase64 = !!(uri && uri.match(/^data:application\/pdf;base64/));
-
-        const cacheFile =
-          RNFetchBlob.fs.dirs.CacheDir + "/" + SHA1(uri) + ".pdf";
-
-        // delete old cache file
-        this._unlinkFile(cacheFile);
-
-        if (isNetwork) {
-          this._downloadFile(source, cacheFile);
-        } else if (isAsset) {
-          RNFetchBlob.fs
-            .cp(uri, cacheFile)
-            .then(() => {
-              if (this._mounted) {
-                this.setState({
-                  path: cacheFile,
-                  isDownloaded: true,
-                  progress: 1,
-                });
-              }
-            })
-            .catch(async (error) => {
-              this._unlinkFile(cacheFile);
-              this._onError(error);
-            });
-        } else if (isBase64) {
-          let data = uri.replace(/data:application\/pdf;base64,/i, "");
-          RNFetchBlob.fs
-            .writeFile(cacheFile, data, "base64")
-            .then(() => {
-              if (this._mounted) {
-                this.setState({
-                  path: cacheFile,
-                  isDownloaded: true,
-                  progress: 1,
-                });
-              }
-            })
-            .catch(async (error) => {
-              this._unlinkFile(cacheFile);
-              this._onError(error);
-            });
-        } else {
-          if (this._mounted) {
-            this.setState({
-              path: uri.replace(/file:\/\//i, ""),
-              isDownloaded: true,
-            });
-          }
-        }
-      } else {
-        this._onError(new Error("no pdf source!"));
-      }
-    } catch (e) {
-      this._onError(e);
-    }
-  };
-
-  _downloadFile = async (source, cacheFile) => {
-    if (this.lastRNBFTask) {
-      this.lastRNBFTask.cancel((err) => {});
-      this.lastRNBFTask = null;
-    }
-
-    const tempCacheFile = cacheFile + ".tmp";
-
-    this._unlinkFile(tempCacheFile);
-
-    this.lastRNBFTask = this.props.fileManager.processDownload();
-
-    RNFetchBlob.config({
-      // response data will be saved to this path if it has access right.
-      path: tempCacheFile,
-      trusty: this.props.trustAllCerts,
-    })
-      .fetch(
-        source.method ? source.method : "GET",
-        source.uri,
-        source.headers ? source.headers : {},
-        source.body ? source.body : ""
-      )
-      // listen to download progress event
-      .progress((received, total) => {
-        this.props.onLoadProgress &&
-          this.props.onLoadProgress(received / total);
-        if (this._mounted) {
-          this.setState({ progress: received / total });
-        }
-      });
-
-    this.lastRNBFTask
-      .then(async (res) => {
-        this.lastRNBFTask = null;
-
-        if (
-          res &&
-          res.respInfo &&
-          res.respInfo.headers &&
-          !res.respInfo.headers["Content-Encoding"] &&
-          !res.respInfo.headers["Transfer-Encoding"] &&
-          res.respInfo.headers["Content-Length"]
-        ) {
-          const expectedContentLength = res.respInfo.headers["Content-Length"];
-          let actualContentLength;
-
-          try {
-            const fileStats = await RNFetchBlob.fs.stat(res.path());
-
-            if (!fileStats || !fileStats.size) {
-              throw new Error("FileNotFound:" + source.uri);
-            }
-
-            actualContentLength = fileStats.size;
-          } catch (error) {
-            throw new Error("DownloadFailed:" + source.uri);
-          }
-
-          if (expectedContentLength != actualContentLength) {
-            throw new Error("DownloadFailed:" + source.uri);
-          }
-        }
-
-        this._unlinkFile(cacheFile);
-        RNFetchBlob.fs
-          .cp(tempCacheFile, cacheFile)
-          .then(() => {
-            if (this._mounted) {
-              this.setState({
-                path: cacheFile,
-                isDownloaded: true,
-                progress: 1,
-              });
-            }
-            this._unlinkFile(tempCacheFile);
-          })
-          .catch(async (error) => {
-            throw error;
-          });
-      })
-      .catch(async (error) => {
-        this._unlinkFile(tempCacheFile);
-        this._unlinkFile(cacheFile);
-        this._onError(error);
-      });
-  };
-
-  _unlinkFile = async (file) => {
-    try {
-      await this.props.fileManager.unlinkFile(file);
+      await this.props.fileManager.unlinkFile(filePath);
     } catch (e) {}
   };
 
@@ -417,7 +211,7 @@ export default class Pdf extends Component {
     return (
       <View style={[this.props.style, { overflow: "hidden" }]}>
         {!this.state.isDownloaded ? (
-          <View style={styles.progressContainer}></View>
+          <View />
         ) : Platform.OS === "android" || Platform.OS === "windows" ? (
           <PdfCustom
             ref={(component) => (this._root = component)}
@@ -471,11 +265,3 @@ if (Platform.OS === "android") {
     nativeOnly: { path: true, onChange: true },
   });
 }
-
-const styles = StyleSheet.create({
-  progressContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
